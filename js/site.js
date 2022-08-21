ae657aac082dfb26ee6414fd778f98eef35b0fc2
Ian-Barton <bobbarbid.1@gmail.com>
2022-08-21 12:47:47 -0500

    Good so far
    I've added the basic layout of the Todo app, and figured out how to do special dropdowns that display colors and icons.

--- js/site.js
+++ js/site.js
@@ -1,3 +1,12 @@
-function HelloWorld() {
-    alert("Hello World");
+//Get Values
+function getValues() {
+    let entry = document.getElementById("").value
+}
+//Set Logic
+function createTask() {
+
+}
+//Display Results
+function display() {
+
 }
