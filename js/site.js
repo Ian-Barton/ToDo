0b663a3ac8ae477beb8289317fc681bb6ae26e39
Ian-Barton <bobbarbid.1@gmail.com>
2022-08-28 17:53:27 -0500

    A large step
    Made about half of the app. Got the basic set up and I'm now working on the code. Now what I'm doing is finding a good way to display tasks and remove them.

--- js/site.js
+++ js/site.js
@@ -1,12 +1,16 @@
 //Get Values
 function getValues() {
-    let entry = document.getElementById("").value
+    let task = document.getElementById("tasks").value;
+    let text = document.createTextNode(task);
+    let task_el = document.createElement("li");
+    task_el.classList.add("list-group");
+    task_el.appendChild(text);
+    display(task_el);
 }
 //Set Logic
 function createTask() {
-
 }
-//Display Results
-function display() {
-
+function display(task_el) { 
+    let task_li = document.getElementById("list");
+    task_li.appendChild(task_el);
 }
